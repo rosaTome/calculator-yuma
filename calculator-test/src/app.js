ad353234ac2a06a98b4105fdfe759c6bde998d17
rosaTome <tomerosalinda@gmail.com>
2024-04-03 07:09:44 +0100

    dom, function, operators and number and decimal elements set in js

---
+++ calculator-test/src/app.js
@@ -0,0 +1,37 @@
+// DOM Elements 
+const displayElement = document.querySelector('.display-number');
+
+// function elements
+const acElement = document.querySelector('.ac');
+const plusminusElement = document.querySelector('.pm');
+const percentElement = document.querySelector('.percentage');
+
+// operators elements 
+const additionElement = document.querySelector('.addition');
+const subtractionElement = document.querySelector('.subtraction');
+const multiplicationElement = document.querySelector('.multiplication');
+const divisionlement = document.querySelector('.division');
+const equalElement = document.querySelector('.equal');
+
+// numbers and decimal elements 
+const decimalElement = document.querySelector('.decimal');
+const number0Element = document.querySelector('.number-0');
+const number1Element = document.querySelector('.number-1');
+const number2Element = document.querySelector('.nimber-2');
+const number3Element = document.querySelector('.number-3');
+const number4Element = document.querySelector('.number-4');
+const number5Element = document.querySelector('.number-5');
+const number6Element = document.querySelector('.number-6');
+const number7Element = document.querySelector('.number-7');
+const number8Element = document.querySelector('.number-8');
+const number9Element = document.querySelector('.number-9');
+
+// numbers array for loop
+const numberElementArray = {
+    number0Element, number1Element, number2Element, number3Element, number4Element, number5Element, number6Element, number7Element, number8Element, number9Element, 
+}
+
+
+
+
+
